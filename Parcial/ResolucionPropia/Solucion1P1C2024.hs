460ddfd060b572d94aa6af349ea79333c7eb6680
ju4nc5r <juan.cruz.astrada@hotmail.com>
2024-06-23 23:08:40 -0300

    Se agrega nuevo ejercicio del ultimo parcial

--- Parcial/ResolucionPropia/Solucion1P1C2024.hs
+++ Parcial/ResolucionPropia/Solucion1P1C2024.hs
@@ -35,14 +35,26 @@ generarStock :: [String] -> [(String, Int)]
 generarStock [] = []
 generarStock (x:xs) = (x, cantApariciones x (x:xs)) : generarStock (quitarApariciones x xs)
 
+-- Ejercicio 2
+{-
+problema stockDeProducto (stock:seq⟨seq⟨Char⟩ x Z⟩, producto: seq⟨Char⟩) : Z {
+requiere: {No hay productos repetidos en stock}
+requiere: {Todas las cantidades de los productos que hay en stock son mayores a cero}
+asegura: {(res = 0 y producto no se encuentra en el stock) o (existe un i tal que 0 <= i < |stock| y producto=stock[i]0
+y res = stock[i]1)}
+}
+-}
 
+pertenece :: String -> [(String,Int)] -> Bool
+pertenece s [] = False
+pertenece s (x:xs) | fst x == s = True
+                   | otherwise = pertenece s xs 
 
+--producto :: String -> [(String,Int)] -> (String,Int)
+--producto s (x:xs) | s == fst x = 
 
-
-
---existe :: String -> [(String,Int)] -> 
---existe s (x:xs) | pertenece s (x:xs) =    
-
--- (x, cantApariciones x (x:xs)) : generarStock xs 
-
+stockDeProducto :: [(String,Int)] -> String -> Int
+stockDeProducto [] s = 0
+stockDeProducto (x:xs) producto | producto == fst x = snd x 
+                                | otherwise = stockDeProducto xs producto
 
