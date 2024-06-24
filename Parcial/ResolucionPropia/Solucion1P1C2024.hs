f2425051050f24d88c880b51faa665eca699a2b0
ju4nc5r <juan.cruz.astrada@hotmail.com>
2024-06-23 23:40:48 -0300

    Se resuelve ejercicio 4

--- Parcial/ResolucionPropia/Solucion1P1C2024.hs
+++ Parcial/ResolucionPropia/Solucion1P1C2024.hs
@@ -50,11 +50,38 @@ pertenece s [] = False
 pertenece s (x:xs) | fst x == s = True
                    | otherwise = pertenece s xs 
 
---producto :: String -> [(String,Int)] -> (String,Int)
---producto s (x:xs) | s == fst x = 
 
 stockDeProducto :: [(String,Int)] -> String -> Int
 stockDeProducto [] s = 0
 stockDeProducto (x:xs) producto | producto == fst x = snd x 
                                 | otherwise = stockDeProducto xs producto
 
+-- Ejercicio 4
+
+{-
+problema aplicarOferta (stock:seq⟨seq⟨Char⟩ x Z⟩, precios: seq⟨seq⟨Char⟩ x R⟩: seq⟨seq⟨Char⟩ x R⟩ {
+requiere: {No hay productos repetidos en stock}
+requiere: {Todas las cantidades de los productos que hay en stock son mayores a cero}
+requiere: {No hay productos repetidos en precios}
+requiere: {Todos los precios de los productos son mayores a cero}
+requiere: {Todo producto en stock aparece en la lista de precios}
+asegura: {|res| = | precios|}
+asegura: {Para todo 0 <= i < |precios| si stockDeProducto(stock, precios[i]0) > 10 entonces res[i]0 = precios[i]0 y
+res[i]1 = precios[i]1 * 0,80}
+asegura: {Para todo 0 <= i < |precios| si stockDeProducto(stock, precios[i]0) <= 10 entonces res[i]0 = precios[i]0 y
+res[i]1 = precios[i]1}
+}
+-}
+
+
+-- Para todo precio si stockDeProducto > 10 entonces precio del producto se actualiza.  
+
+aplicarOferta :: [(String,Int)] -> [(String,Int)] -> [(String,Int)]
+aplicarOferta [] [] = []
+aplicarOferta (x:xs) (y:ys) | stockDeProducto (x:xs) (fst x) > 10 = (fst x, nuevoPrecio (snd y)) : aplicarOferta xs ys
+                            | otherwise = (fst x, snd y) : aplicarOferta xs ys
+
+
+-- 0.8 * 5 = 4 
+nuevoPrecio :: Int -> Int
+nuevoPrecio x = (x * 4) `div` 5
